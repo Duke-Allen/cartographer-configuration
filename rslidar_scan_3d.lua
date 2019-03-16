1d872376c7ae1fbc3b52bcdc2f4e18b325735fb1
XiaoRui <45333787+Duke-Allen@users.noreply.github.com>
2019-03-16 11:50:51 +0800

    Update rslidar_scan_3d.lua

--- rslidar_scan_3d.lua
+++ rslidar_scan_3d.lua
@@ -49,14 +49,14 @@ TRAJECTORY_BUILDER_2D.max_z = 1.0
 TRAJECTORY_BUILDER_3D.use_online_correlative_scan_matching = false
 
 MAP_BUILDER.use_trajectory_builder_3d = true
-MAP_BUILDER.num_background_threads = 7
+MAP_BUILDER.num_background_threads = 4
 POSE_GRAPH.optimization_problem.huber_scale = 5e2
 POSE_GRAPH.optimization_problem.log_solver_summary = true
 POSE_GRAPH.optimize_every_n_nodes = 40
 POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 20
 POSE_GRAPH.constraint_builder.sampling_ratio = 0.03
-POSE_GRAPH.constraint_builder.min_score = 0.62
-POSE_GRAPH.constraint_builder.global_localization_min_score = 0.66
+POSE_GRAPH.constraint_builder.min_score = 0.5
+POSE_GRAPH.constraint_builder.global_localization_min_score = 0.55
 
 
 return options
